91cce0f7d24b0176e39d36f44877805b6fd4d6a7
AyushSN <ayushsanjivnaidu@gmail.com>
2019-06-11 14:58:21 +0530

    Fixed a bug caused by whitespaces in the ROOT path

--- helper-scripts/build-headers.py
+++ helper-scripts/build-headers.py
@@ -7,7 +7,7 @@ headers = os.listdir(ROOT + "\\include\\BankSystem")
 
 for h in headers:
     h_name = h[:-4]    
-    file_path = ROOT + "\\src\\" + h_name + ".cpp"
+    file_path = ".\\src\\" + h_name + ".cpp"
     if os.path.isfile(file_path):
         cmd = "g++ -c -o .\\build\\" + h_name + ".o " + file_path
         os.system(cmd)
